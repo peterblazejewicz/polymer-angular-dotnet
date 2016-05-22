9a2c94e544fea2a8907e8c771a43fc119ae15431
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-05-23 00:08:50 +0200

    Small fix to controller attribs

--- src/gulp-dotnet-browser-sync/ShopWebApi/ShopAPI/Controllers/ValuesController.cs
+++ src/gulp-dotnet-browser-sync/ShopWebApi/ShopAPI/Controllers/ValuesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ShopAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/values")]
     public class ValuesController : Controller
     {
         // GET api/values
