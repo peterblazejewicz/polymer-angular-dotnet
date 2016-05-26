12515a50f4bec558daba0b53fa5b711be70d4112
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-05-27 00:03:16 +0200

    Prepare json data cont.

--- src/gulp-dotnet-browser-sync/Shop/Controllers/CategoriesController.cs
+++ src/gulp-dotnet-browser-sync/Shop/Controllers/CategoriesController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Shop.Controllers
@@ -5,12 +6,17 @@ namespace Shop.Controllers
     [Route("api/categories")]
     public class CategoriesController : Controller
     {
+        public IHostingEnvironment HostingEnvironment { get; set; }
+        public CategoriesController(IHostingEnvironment env)
+        {
+            HostingEnvironment = env;
+        }
 
         // GET api/categories/{id}
         [HttpGet("{id}")]
         public string Get(string id)
         {
-            return "value";
+            return "tbd";
         }
 
     }
