93573b4cc89222013f1d99da8c35b71be518e39f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-05 00:20:03 +0200

    Reset C#

--- src/zuperkulblog/Program.cs
+++ src/zuperkulblog/Program.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
 
 namespace ZuperKulBlog
 {
@@ -8,19 +12,13 @@ namespace ZuperKulBlog
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-              .SetBasePath(Directory.GetCurrentDirectory())
-              .AddJsonFile("hosting.json", optional: false)
-              .AddEnvironmentVariables(prefix: "ASPNETCORE_")
-              .AddCommandLine(args)
-              .Build();
             var host = new WebHostBuilder()
-                .UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
             host.Run();
         }
     }
