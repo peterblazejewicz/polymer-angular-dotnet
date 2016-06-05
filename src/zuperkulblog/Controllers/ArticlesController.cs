ea127005f977f9ad50a93d4ddd3de1175ffc3ec1
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-06-05 23:25:06 +0200

    Add method to get blog article by category and id

--- src/zuperkulblog/Controllers/ArticlesController.cs
+++ src/zuperkulblog/Controllers/ArticlesController.cs
@@ -30,5 +30,32 @@ namespace ZuperKulBlog.Controllers
             return categories;
         }
 
+        // GET api/articles/{category}
+        /// <summary>
+        /// Get article for given category and id
+        /// </summary>
+        /// <param name=""{category:alpha}/{id:int}""></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("{category:alpha}/{id:int}")]
+        public object GetArticle(string category, int id)
+        {
+            string path = Path.Combine(HostingEnvironment.WebRootPath, "data", "articles.json");
+            if (System.IO.File.Exists(path) == false)
+            {
+                return NotFound("Can't find backing data file");
+            }
+            IEnumerable<Category> categories = JsonConvert.DeserializeObject<IEnumerable<Category>>(System.IO.File.ReadAllText(path));
+            Category match = categories
+                .Where(c => c.Name.Equals(category, StringComparison.CurrentCultureIgnoreCase))
+                .First();
+            if(match == null) return NotFound();
+            if(match.Articles == null) return NotFound();
+            var article = match.Articles
+                .Where(a => a.Id == id).First();
+            if(article == null) return NotFound();
+            return article;
+        }
+
     }
 }
